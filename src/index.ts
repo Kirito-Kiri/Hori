7a307a005987d6c2d8565ff3ab60153a4e21638b
Kirito-Kiri <kiritokiri@protonmail.com>
2020-11-11 22:57:37 +0530

    method to import config

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import { prefix, token } from '../config.json'
+import { Client } from 'discord.js'
+//import fs from 'fs'
+import { loadConfig } from "./utils"
+const config = loadConfig()
 
+const client: Client = new Client()
+
+client.on('ready',() => {
+    console.log(`${client.user.username}`)
+}) 
+
+client.login(config.token)
---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+import fs from "fs"
+import path from "path"
+interface BotConfig {
+    prefix: string,
+    token: string
+}
+export const loadConfig = () => {
+    const file = fs.readFileSync(path.join(__dirname, "..", "config.json"), {encoding: "utf8"})
+    return JSON.parse(file) as BotConfig
+}
