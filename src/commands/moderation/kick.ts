0f58ef7ada1dda206b123e24a35b1158b6ad4907
Kirito-Kiri <kiritokiri@protonmail.com>
2020-12-08 23:06:32 +0530

    basic Kick

--- src/commands/moderation/kick.ts
+++ src/commands/moderation/kick.ts
@@ -13,8 +13,25 @@ export default class Kick extends Command {
   }
 
   public async run(msg: Message, args: string[]) {
-    await msg.reply(`owo`)
-    const reason = args
-    console.log(reason)
+    const user = msg.mentions.users.first()
+    const reason = args.toString()
+    if (user) {
+      const member = msg.guild.member(user)
+      if (member) {
+        await member
+          .kick(reason)
+          .then(() => {
+            msg.reply(`Kicked ${user.username} succesfully`)
+          })
+          .catch(err => {
+            msg.reply('I was unable to kick the user')
+            console.error(err)
+          })
+      } else {
+        msg.reply('The user is not a member of the guild')
+      }
+    } else {
+      msg.reply('Please mention a user')
+    }
   }
 }
