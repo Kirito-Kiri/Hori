af94540b015228222b6a4cc976719f4f515b9674
Kirito-Kiri <kiritokiri@protonmail.com>
2020-12-16 13:39:38 +0530

    h!say <text> | Testing Owner only command

---
+++ src/commands/owner/say.ts
@@ -0,0 +1,24 @@
+import { Message } from 'discord.js'
+import { Command } from '../../Structure/Command'
+
+export default class say extends Command {
+  public constructor() {
+    super({
+      name: 'say',
+      description: 'Repeats what you said and deletes the command',
+      argumentRequired: true,
+      isGuildOnly: true,
+      isOwnerOnly: true,
+      permsNeeded: ['MANAGE_MESSAGES']
+    })
+  }
+
+  public async run(msg: Message, args: string) {
+    try {
+      await msg.delete()
+      await msg.channel.send(args)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
