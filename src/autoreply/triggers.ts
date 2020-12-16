31cfc542a291d22a355aa88606855076edc0306a
Kirito-Kiri <kiritokiri@protonmail.com>
2020-12-16 17:35:56 +0530

    replies oof now

--- src/autoreply/triggers.ts
+++ src/autoreply/triggers.ts
@@ -1,5 +1,8 @@
 import { Message } from 'discord.js'
-
+/**
+ * Sends a message on matching trigger
+ * @param msg Message sent by user
+ */
 export const autoreply = (msg: Message) => {
   if (msg.author.bot) return
   const triggers = ['Pat', 'Nyaa']
