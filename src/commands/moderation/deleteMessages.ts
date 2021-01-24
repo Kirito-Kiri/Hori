383728a6db20cee757b6f8d998e24ddf0a688db3
Kirito-Kiri <kiritokiri@protonmail.com>
2021-01-25 01:22:56 +0530

    New command to nuke chats

---
+++ src/commands/moderation/deleteMessages.ts
@@ -0,0 +1,28 @@
+import { Message, TextChannel } from 'discord.js'
+import { Command } from '../../Structure/Command'
+
+export default class DeleteMessages extends Command {
+  public constructor() {
+    super({
+      name: 'nuke',
+      description: 'Deletes x messages as provided in argument',
+      argumentRequired: true,
+      isGuildOnly: true,
+      permsNeeded: ['MANAGE_MESSAGES']
+    })
+  }
+
+  public async run(msg: Message, args: string) {
+    const num = Number(args[0])
+    msg.delete()
+    if (!isNaN(num) && msg.channel instanceof TextChannel) {
+      try {
+        return await msg.channel.bulkDelete(num, true)
+      } catch (err) {
+        console.error(err)
+      }
+    } else {
+      return
+    }
+  }
+}
